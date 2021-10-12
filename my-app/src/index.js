5e638fd7a5e7d6e0add0d62ad9f526261765c525
unknown <nikita.waara@hotmail.com>
2021-10-12 11:14:42 +0300

    inital commit

---
+++ my-app/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Card from './Card';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Card />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
